804ec008bae4793ad9c6c99d419e392e93d6e43f
art-inko <art-inko@yandex.ru>
2016-02-21 15:03:54 +0300

    пустое поле

--- SeaBattle/src/Game.java
+++ SeaBattle/src/Game.java
@@ -8,26 +8,28 @@ public class Game {
     public void startGame() {
         Field playerField = new Field();
         Field PCField = new Field();
+        Field PCHiddenField = new Field();
         playerField.initPlayerField("[ ]");
         PCField.initPlayerField("[ ]");
+        PCHiddenField.initPlayerField("[ ]");
         playerField.setShips();
         PCField.setShips();
-        printDoubleField(playerField, PCField);
+        printDoubleField(playerField, PCHiddenField);
 
         // Вечный цикл до выигрыша
     }
 
-    public void printDoubleField(Field playerField, Field PCField) {
+    public void printDoubleField(Field field1, Field Field2) {
         System.out.println("            ***Поле 1го игрока***                                  ***Поле 2го игрока***  ");
         System.out.println();
         for (int i = 0; i < 11; i++) {
             for (int j = 0; j < 11; j++) {
-                System.out.print(playerField.getField(i, j) + " ");
+                System.out.print(field1.getField(i, j) + " ");
                 if (j >= 10) {
                     System.out.print("          ");
 
                     for (int d = 0; d < 11; d++) {
-                        System.out.print(PCField.getField(i, d) + " ");
+                        System.out.print(Field2.getField(i, d) + " ");
                     }
                 }
             }
