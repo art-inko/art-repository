4af83ec36cc8480f12096dc4f2a2446968f9fe5e
Artem <art-inko@yandex.ru>
2016-03-01 10:34:03 +0300

    Делаем статус выстрела, ранил, убил

---
+++ SeaBattle/src/objects/stShip.java
@@ -0,0 +1,23 @@
+package objects;
+
+/**
+ * Created by Art-PC on 29.02.2016.
+ */
+public class stShip {
+    int X1;
+    int X2;
+    int Y1;
+    int Y2;
+    boolean isHorizontal;
+    int size;
+
+
+    //    public stShip(int x1, int x2, int y1, int y2, boolean isHorizontal, int size) {
+//        X1 = x1;
+//        X2 = x2;
+//        Y1 = y1;
+//        Y2 = y2;
+//        this.isHorizontal = isHorizontal;
+//        this.size = size;
+//    }
+}
